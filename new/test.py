60ad4a31d836ba0338d34e8af5834836ef05821b
harshvardhangaddam@gmail.com <harshvardhangaddam@gmail.com>
2023-06-17 18:57:22 +0530

    test

---
+++ new/test.py
@@ -0,0 +1,8 @@
+from tkinter import filedialog
+from tkinter import *
+
+root = Tk()
+root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+root.after(1,lambda:root.destroy())
+print (root.filename)
+
